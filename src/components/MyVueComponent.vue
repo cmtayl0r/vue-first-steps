9b34c693115beeec3cd0b23b38a22757747da42a
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-10-14 19:00:44 +0200

    add v-for and attribute binding examples

--- src/components/MyVueComponent.vue
+++ src/components/MyVueComponent.vue
@@ -1,4 +1,4 @@
-// TEMPLATE
+<!-- TEMPLATE -->
 <template>
   <h2>The count is {{ count }}</h2>
   <button @click="increment">UP ⬆️</button>
@@ -13,6 +13,28 @@
     <button @click="toggleShowLorem">{{ buttonLabel }}</button>
   </div>
   <br />
+
+  <!-- v-for and Attribute binding -->
+  <h3>All books</h3>
+  <ul>
+    <li
+      v-for="book in books"
+      :class="{ fav: book.isFav }"
+      @click="toggleFav(book)"
+    >
+      <h3>{{ book.title }}</h3>
+      <p>{{ book.author }}</p>
+      <img :src="book.img" :alt="book.title" />
+    </li>
+  </ul>
+  <h3>Favourite books</h3>
+  <ul>
+    <li v-for="book in favBooks">
+      <h3>{{ book.title }}</h3>
+      <p>{{ book.author }}</p>
+      <img :src="book.img" :alt="book.title" />
+    </li>
+  </ul>
   <!-- Mouse events -->
   <!-- define $event to access the event object with other arguments -->
   <div class="box" @mouseover="handleEvent($event, 5)">mouseover</div>
@@ -23,9 +45,39 @@
   </div>
 </template>
 
-// SCRIPT SETUP
+<!-- SCRIPT -->
 <script setup>
 import { ref, computed } from "vue";
+
+// Books
+const books = ref([
+  {
+    title: "name of the wind",
+    author: "patrick rothfuss",
+    img: "assets/1.jpg",
+    isFav: true,
+  },
+  {
+    title: "the way of kings",
+    author: "brandon sanderson",
+    img: "assets/2.jpg",
+    isFav: false,
+  },
+  {
+    title: "the final empire",
+    author: "brandon sanderson",
+    img: "assets/3.jpg",
+    isFav: true,
+  },
+]);
+// Methods
+const toggleFav = (book) => {
+  book.isFav = !book.isFav; // toggle the isFav property
+};
+// Computed
+const favBooks = computed(() => {
+  return books.value.filter((book) => book.isFav);
+});
 
 // Count
 const count = ref(0); // ref() is a Vue 3 function that creates a reactive reference
@@ -62,7 +114,7 @@ const handleMouseMove = (e) => {
 };
 </script>
 
-// STYLES
+<!-- STYLES -->
 <style scoped>
 .box {
   padding: 100px;
@@ -71,5 +123,19 @@ const handleMouseMove = (e) => {
   background-color: antiquewhite;
   margin: 20px;
   display: inline-block;
+}
+li {
+  list-style-type: none;
+  background: #fff;
+  margin: 20px auto;
+  padding: 10px 20px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+li.fav {
+  background: #ff9ed2;
+  color: white;
 }
 </style>
