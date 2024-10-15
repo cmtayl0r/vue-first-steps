9123582e074460f7bbd603a516c0781d515ef895
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-10-15 13:22:00 +0200

    add refactored MyVueComponent

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
 <template>
-  <!-- <MyVueComponent /> -->
-  <h1>My first Vue App</h1>
-  <input type="text" ref="name" />
-  <button @click="handleClick">Click me</button>
-  <Modal />
+  <h1>Vue Jobs</h1>
 </template>
 
-<script setup>
-import { ref } from "vue";
-import MyVueComponent from "./components/MyVueComponent.vue";
-import Modal from "./components/Modal.vue";
+<script setup></script>
 
-const name = ref(null);
-
-const handleClick = () => {
-  console.log("clicked");
-  name.value.focus();
-  name.value.classList.add("active");
-};
-</script>
-
-<style scoped></style>
+<style lang="scss" scoped></style>
--- src/components/MyVueComponent.vue
+++ src/components/MyVueComponent.vue
@@ -1,141 +1,73 @@
-<!-- TEMPLATE -->
 <template>
-  <h2>The count is {{ count }}</h2>
-  <button @click="increment">UP ⬆️</button>
-  <button @click="decrement">DOWN ⬇️</button>
+  <h1>Vue Test</h1>
+  <h3>{{ name }}</h3>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
 
-  <p v-show="showLorem">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sequi dolorum
-    maiores, suscipit dolores illum, necessitatibus unde placeat officiis quasi
-    deserunt amet. Cum debitis amet ut quibusdam pariatur voluptatum harum?
-  </p>
-  <div>
-    <button @click="toggleShowLorem">{{ buttonLabel }}</button>
-  </div>
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <!-- input bound to newTask variable -->
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
   <br />
-
-  <!-- v-for and Attribute binding -->
-  <h3>All books</h3>
-  <ul>
-    <li
-      v-for="book in books"
-      :class="{ fav: book.isFav }"
-      @click="toggleFav(book)"
-    >
-      <h3>{{ book.title }}</h3>
-      <p>{{ book.author }}</p>
-      <img :src="book.img" :alt="book.title" />
-    </li>
-  </ul>
-  <h3>Favourite books</h3>
-  <ul>
-    <li v-for="book in favBooks">
-      <h3>{{ book.title }}</h3>
-      <p>{{ book.author }}</p>
-      <img :src="book.img" :alt="book.title" />
-    </li>
-  </ul>
-  <!-- Mouse events -->
-  <!-- define $event to access the event object with other arguments -->
-  <div class="box" @mouseover="handleEvent($event, 5)">mouseover</div>
-  <div class="box" @mouseleave="handleEvent">mouseleave</div>
-  <div class="box" @dblclick="handleEvent">double click</div>
-  <div class="box" @mousemove="handleMouseMove">
-    Position {{ x }} and {{ y }}
-  </div>
+  <button @click="toggleStatus">Toggle Status</button>
 </template>
 
-<!-- SCRIPT -->
 <script setup>
-import { ref, computed } from "vue";
+// IMPORTS
+import { onMounted, ref } from "vue";
 
-// Books
-const books = ref([
-  {
-    title: "name of the wind",
-    author: "patrick rothfuss",
-    img: "assets/1.jpg",
-    isFav: true,
-  },
-  {
-    title: "the way of kings",
-    author: "brandon sanderson",
-    img: "assets/2.jpg",
-    isFav: false,
-  },
-  {
-    title: "the final empire",
-    author: "brandon sanderson",
-    img: "assets/3.jpg",
-    isFav: true,
-  },
-]);
-// Methods
-const toggleFav = (book) => {
-  book.isFav = !book.isFav; // toggle the isFav property
-};
-// Computed
-const favBooks = computed(() => {
-  return books.value.filter((book) => book.isFav);
-});
+// REACTIVE VARIABLES
+const name = ref("Chris Taylor");
+const status = ref("active");
+const tasks = ref(["task1", "task2", "task3"]);
+const newTask = ref(""); // new task input
 
-// Count
-const count = ref(0); // ref() is a Vue 3 function that creates a reactive reference
-const increment = () => count.value++;
-const decrement = () => count.value--;
-
-// Show/hide lorem text
-const showLorem = ref(true);
-const toggleShowLorem = () => {
-  showLorem.value = !showLorem.value;
-};
-const buttonLabel = computed(() =>
-  showLorem.value ? "Hide Lorem" : "Show Lorem"
-);
-
-// Mouse events
-const handleEvent = (e, data) => {
-  console.log(e.type);
-  if (data) {
-    console.log(data);
+//METHODS
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
   }
 };
-const x = ref(0);
-const y = ref(0);
-const handleMouseMove = (e) => {
-  x.value = e.offsetX;
-  y.value = e.offsetY;
 
-  // Reset to 0 on mouse leave
-  e.target.addEventListener("mouseleave", () => {
-    x.value = 0;
-    y.value = 0;
-  });
+const addTask = () => {
+  if (newTask.value !== "") {
+    // check if new task input is not empty
+    tasks.value.push(newTask.value); // add new task to tasks array
+    newTask.value = ""; // clear new task input
+  }
 };
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1); // remove task from tasks array
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+
+    // map through data and return only the title of each task
+    // set the tasks array to the new array of task titles
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.error(error);
+  }
+});
 </script>
 
-<!-- STYLES -->
-<style scoped>
-.box {
-  padding: 100px;
-  width: 400px;
-  text-align: center;
-  background-color: antiquewhite;
-  margin: 20px;
-  display: inline-block;
-}
-li {
-  list-style-type: none;
-  background: #fff;
-  margin: 20px auto;
-  padding: 10px 20px;
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-li.fav {
-  background: #ff9ed2;
-  color: white;
-}
-</style>
+<style scoped></style>
